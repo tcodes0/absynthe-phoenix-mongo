5a65d31e9cdee0c61b7389be6194d6a458441523
Raphael Thomazella <thomazella9@gmail.com>
2019-11-11 21:11:03 -0300

    chore(misc): mutations

--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -1,19 +1,29 @@
 defmodule CommunityWeb.Schema do
   use Absinthe.Schema
-  # alias CommunityWeb.NewsResolver
+  alias CommunityWeb.NewsResolver
 
-  # object :link do
-  #   field :id, non_null(:id)
-  #   field :url, non_null(:string)
-  #   field :description, non_null(:string)
-  # end
+  object :link do
+    field(:id, non_null(:id))
+    field(:url, non_null(:string))
+    field(:description, non_null(:string))
+  end
 
   query do
-    # field :all_links, non_null(list_of(non_null(:link))) do
-    #   resolve &NewsResolver.all_links/3
-    # end
+    field :all_links, non_null(list_of(non_null(:link))) do
+      resolve(&NewsResolver.all_links/3)
+    end
+
     field :hello, :string do
-      resolve fn _, _ -> {:ok, "world!"} end
+      resolve(fn _, _ -> {:ok, "world!"} end)
+    end
+  end
+
+  mutation do
+    field :create_link, :link do
+      arg(:url, non_null(:string))
+      arg(:description, non_null(:string))
+
+      resolve(&NewsResolver.create_link/3)
     end
   end
 end
